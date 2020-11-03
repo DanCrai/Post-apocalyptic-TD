a791866c5913f223c4d48e8675901a68dfb22106
Theodorson <48293392+Theodorson@users.noreply.github.com>
2020-11-03 17:17:50 +0200

    Spawn

---
+++ Enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+
+
+    void Start ()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update ()
+    {
+      
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.4f)
+        {
+            GetNextWayPoint();
+        }
+
+    }
+
+    void GetNextWayPoint()
+    {
+        if ( wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            wavepointIndex++;
+            target = Waypoints.points[wavepointIndex];
+        }
+
+    }
+}
---
+++ WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemy;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 6f;
+    public float timeBetweenEnemies = 0.8f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {   
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+        waveNumber++;
+        //Debug.Log("Spawn wave succed");
+
+    }
+
+    void SpawnEnemy()
+    {
+
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+
+    }
+
+}
